3e9c2605cd60fb1ff325e594e34e260279230122
Sajjad Rezai <rezai_sajjad@yahoo.com>
2023-12-17 18:32:36 +0330

    Update User.py

--- src/models/User.py
+++ src/models/User.py
@@ -1,11 +1,10 @@
-
 from datetime import date
-
 
 class User(object):
     """
     docstring
     """
+
     id: int
     user_Name: str
     name: str
@@ -14,17 +13,30 @@ class User(object):
     email: str
     birth_day: date
 
+
 class UserPassword(object):
+    """
+    docstring
+    """
+
     id: int
     password: str
 
 
 class Question(object):
+    """
+    docstring
+    """
+
     id: int
     text: str
 
 
 class UserQuestion(object):
+    """
+    docstring
+    """
+
     id: int
     question_id: int
     answer: str
