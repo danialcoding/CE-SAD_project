b727e07ee262967043c6887b45e63d83b985e961
Sajjad Rezai <rezai_sajjad@yahoo.com>
2023-12-18 17:08:13 +0330

    Update user.py

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI
-from models.user import User
-from services import unit_of_work
+from models.user import Login, User
+from services.unit_of_work import UnitOfWork
 
 
-def add_controller(app: FastAPI, uof: unit_of_work):
+def add_controller(app: FastAPI, uof: UnitOfWork):
     """
     docstring
     """
-    list = []
 
-    @app.get("/users/")
-    async def add_item():
-        return list
+    @app.get("/api/users/")
+    async def get_users(count: int, offset: int = 0):
+        return uof.users.get(count, offset)
 
-    @app.post("/users/")
-    async def create_item(item: User):
-        list.append(item)
-        return None
+    @app.post("/api/users/")
+    async def insert_users(user: User):
+        uof.users.insert(user)
+        uof.commit()
 
-    # Import the sqlite3 module
+    @app.get("/api/users/check/username")
+    async def user_check_user_name_available(user_name: str):
+        return uof.users.check_user_name(user_name)
+
+    @app.get("/api/users/check/email")
+    async def user_check_email_available(email: str):
+        return uof.users.check_email(email)
+
+    @app.get("/api/users/check/phone_number")
+    async def user_check_phone_number_available(phone_number: str):
+        return uof.users.check_phone_number(phone_number)
