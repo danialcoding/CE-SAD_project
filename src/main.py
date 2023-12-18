6dc9260be028ddf1319550bf00a14794b355ad5c
Sajjad Rezai <rezai_sajjad@yahoo.com>
2023-12-18 17:08:16 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
-from Services.UnitOfWork import UnitOfWork
+from services.unit_of_work import UnitOfWork
+from fastapi import FastAPI
+import controllers.user
+import controllers.login
+import controllers.dev
 
-ur = UnitOfWork("testSajjad")
-ur.users.insert(User)
-ur.users.getUsers(5)
+app = FastAPI()
+
+uow = UnitOfWork("ce_database")
+
+
+controllers.dev.add_controller(app, uow)
+controllers.user.add_controller(app, uow)
+controllers.login.add_controller(app, uow)
