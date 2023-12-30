965f22dfe3c29025886423556b03afa028d8e771
Sajjad Rezai <rezai_sajjad@yahoo.com>
2023-12-30 23:09:59 +0330

    Update user.py

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,6 +1,8 @@
 import io
 import os
+from typing import IO
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from models.user import Login, User
 from services.unit_of_work import UnitOfWork
 from fastapi import FastAPI, File, UploadFile
@@ -12,17 +14,17 @@ def add_controller(app: FastAPI, uof: UnitOfWork):
     """
 
     @app.get("/api/users/")
-    async def get_users(count: int, offset: int = 0):
+    async def get_users(count: int = 10, offset: int = 0):
         return uof.users.get(count, offset)
-
-    @app.get("/api/users/{user_name}")
-    async def get_users(user_name: str):
-        return uof.users.get_by_user_name(user_name)
 
     @app.post("/api/users/")
     async def insert_users(user: User):
         uof.users.insert(user)
         uof.commit()
+
+    @app.get("/api/users/{user_name}")
+    async def get_user(user_name: str):
+        return uof.users.get_by_user_name(user_name)
 
     @app.get("/api/users/check/username")
     async def user_check_user_name_available(user_name: str):
@@ -36,13 +38,13 @@ def add_controller(app: FastAPI, uof: UnitOfWork):
     async def user_check_phone_number_available(phone_number: str):
         return uof.users.check_phone_number(phone_number)
 
-    @app.get("/api/users/get/question")
-    async def user_get_question(email: str):
-        return uof.users.get_question_by_email(email)
-
-    @app.get("/api/users/validate/forgotpassword")
+    @app.get("/api/users/ckeck/forgotpassword")
     async def user_validate_forgotpassword(email: str, answer: str):
         return uof.users.check_question_answer(email, answer)
+
+    @app.get("/api/users/question")
+    async def user_get_question(email: str):
+        return uof.users.get_question_by_email(email)
 
     @app.get("/api/users/change-password")
     async def user_change_password(email: str, password: str):
@@ -50,20 +52,33 @@ def add_controller(app: FastAPI, uof: UnitOfWork):
         uof.commit()
         return res
 
-    @app.post("/api/users/image")
-    async def user_change_image(file: UploadFile,username:str ,extention: str):
-        uid = uof.users.get_id_by_user_name(username)
-        f = open(f"{uid}.{extention}",'wb')
-        f.write(file.file.read())
-        f.flush()
-        f.close()
-        return {"file_size": file.size}
+    _path = "src\images"
 
-    @app.get("/api/users/{user_name}/image")
-    async def user_get_image(user_name:str):
+    def _findImage(user_name: str):
         uid = uof.users.get_id_by_user_name(user_name)
-        dir = os.listdir()
-        for i in dir:
-            if i.startswith(str(uid)):
-                dir = i
-        return res
+        for i in os.listdir(_path):
+            if i.split(".")[0] == str(uid):
+                return (uid, os.path.join(_path, i))
+        return (uid, None)
+
+    @app.get("/api/users/{user_name}/image", response_class=FileResponse)
+    async def user_get_image(user_name: str):
+        uid, path = _findImage(user_name)
+        if path is None:
+            path = os.path.join(_path, "0.png")
+        f: io.TextIOWrapper = open(path, "r")
+        # fr = FileResponse()
+        # fr.filename = path
+
+        return path
+
+    @app.post("/api/users/{user_name}/image")
+    async def user_change_image(file: UploadFile, user_name: str):
+        extention = file.filename.split(".")[-1]
+        uid, path = _findImage(user_name)
+        if path is not None:
+            os.remove(path)
+        with open(f"src/images/{uid}.{extention}", "wb") as f:
+            f.write(file.file.read())
+            f.flush()
+        return True
